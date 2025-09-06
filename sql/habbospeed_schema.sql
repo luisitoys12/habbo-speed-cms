8eb8e9af76f956de9eb828c9d7a3e9a067ee60ab
Luis Martinez Sandoval <in3707989@gmail.com>
2025-09-05 20:49:52 -0600

    Add tables for slides and DJs agenda to schema

--- sql/habbospeed_schema.sql
+++ sql/habbospeed_schema.sql
@@ -18,8 +18,19 @@ CREATE TABLE djs (
   habbo_user VARCHAR(255),
   horario VARCHAR(255)
 );
+
 CREATE TABLE slides (
   id INT AUTO_INCREMENT PRIMARY KEY,
   archivo VARCHAR(255),
   fecha_subida DATE
+);
+
+CREATE TABLE djs_agenda (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  dj_id INT NOT NULL,
+  fecha DATE NOT NULL,
+  hora_inicio TIME NOT NULL,
+  hora_fin TIME NOT NULL,
+  programa VARCHAR(100),
+  FOREIGN KEY (dj_id) REFERENCES djs(id)
 );
