c2ad9fb008b744fa0cc26922c758d5efe910696d
Luis Martinez Sandoval <in3707989@gmail.com>
2025-09-05 21:49:09 -0600

    Add maintenance mode message in maintenance.php

---
+++ maintenance.php
@@ -0,0 +1,9 @@
+<?php
+// Mostrar solo si está activado
+$activo = true;
+
+if ($activo) {
+  echo "<h1>🚧 Habbospeed está en mantenimiento</h1>";
+  echo "<p>Estamos ajustando los cables y calibrando los beats. Vuelve pronto 🎶</p>";
+  exit();
+}
