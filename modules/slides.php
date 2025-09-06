7f953813b5e640de2c58a90e9d8310ef4bf712b4
Luis Martinez Sandoval <in3707989@gmail.com>
2025-09-05 19:17:23 -0600

    Add slides module with image gallery

---
+++ modules/slides.php
@@ -0,0 +1,8 @@
+<div class="slides">
+  <h2>📸 Galería</h2>
+  <div class="slide-container">
+    <img src="assets/images/slides/slide1.png" alt="Slide 1">
+    <img src="assets/images/slides/slide2.png" alt="Slide 2">
+    <img src="assets/images/slides/slide3.png" alt="Slide 3">
+  </div>
+</div>
