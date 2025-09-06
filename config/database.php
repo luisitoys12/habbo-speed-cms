e777dbbe046c91e3cb5e604f98a3b1047bb3243e
Luis Martinez Sandoval <in3707989@gmail.com>
2025-09-05 22:52:31 -0600

    Update database connection credentials

--- config/database.php
+++ config/database.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("localhost", "usuario", "contraseña", "habbospeed");
+$conn = new mysqli("localhost", "phdcpjffyn", "x5c78bvHZG", "phdcpjffyn");
 
 if ($conn->connect_error) {
   die("Error de conexión: " . $conn->connect_error);
